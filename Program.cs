194e230f8f6d81ed65a89e2d46803d3c94893355
Cecilee Blackburn <cecileeblackburn@gmail.com>
2022-05-14 21:49:38 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+//This is a game called HILO
+//Created by Cecilee Blackburn 
+
+namespace cse210_02
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Player player = new Player();
+            bool result;
+
+            while (player.continuePlaying)
+            {
+                result = letsPlay();
+                player.updatePoints(result);
+                Console.WriteLine($"Your score is: {player.points}");
+                player.setContinuePlaying();
+
+            }
+            Console.WriteLine("Thanks for playing!");
+        }
+            
+    }
+    }
+
